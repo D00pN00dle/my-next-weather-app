8d025a7472b6cd2ec9bbf68a8ecdb693f3df0d19
d00pn00dl3 <hypemodedesigns@protonmail.com>
2025-02-22 18:08:13 -0500

    initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
+    distDir: 'build',
+    basePath: '/my-next-weather-app',
+    assetPrefix: '/my-next-weather-app/',
     images: {
         unoptimized: true,
     },
