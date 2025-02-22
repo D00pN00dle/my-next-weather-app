033f3a8578af88d122df2cec60d843cee50bc9fb
d00pn00dl3 <hypemodedesigns@protonmail.com>
2025-02-22 10:39:04 -0500

    Fix basePath and assetPrefix for GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Ensures Next.js builds a static export
-    basePath: process.env.NODE_ENV === 'production' ? '/my-next-weather-app' : '', // Conditionally set for production
-    assetPrefix: process.env.NODE_ENV === 'production' ? '/my-next-weather-app/' : '', // Conditionally set for production
+    basePath: '/my-next-weather-app', // Conditionally set for production
+    assetPrefix: '/my-next-weather-app/', // Conditionally set for production
     trailingSlash: true, // Adds trailing slashes to URLs
     images: {
       unoptimized: true,
