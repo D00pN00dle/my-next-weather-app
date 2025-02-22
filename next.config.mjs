a0173e4794a31f302b394dfb67e685273d28662f
d00pn00dl3 <hypemodedesigns@protonmail.com>
2025-02-21 19:12:35 -0500

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    output: 'export',
-    basePath: '/my-next-weather-app',
+    output: 'export', // Ensures Next.js builds a static export
+    basePath: '/my-next-weather-app', // GitHub Pages serve from a subdirectory
+    assetPrefix: '/my-next-weather-app/', // Needed for loading assets correctly
+    trailingSlash: true, // Adds trailing slashes to URLs
     images: {
         unoptimized: true,
     },
