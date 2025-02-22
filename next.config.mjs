39d2b60e77cbaa883aff12d043a4bf6cb0c7ffab
d00pn00dl3 <hypemodedesigns@protonmail.com>
2025-02-22 05:09:26 -0500

    update next.config

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export', // Ensures Next.js builds a static export
-    basePath: '/my-next-weather-app', // GitHub Pages serve from a subdirectory
-    assetPrefix: '/my-next-weather-app/', // Needed for loading assets correctly
     trailingSlash: true, // Adds trailing slashes to URLs
     images: {
         unoptimized: true,
