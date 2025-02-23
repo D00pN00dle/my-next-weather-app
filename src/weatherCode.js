fb54800f42864ca55cd67287418434e7c6eeba29
d00pn00dl3 <hypemodedesigns@protonmail.com>
2025-02-23 11:54:23 -0500

    fix weather icon pathes

--- src/weatherCode.js
+++ src/weatherCode.js
@@ -34,169 +34,169 @@ export class weatherCode {
             45: {
                 description: 'Fog',
                 image: {
-                    day: '/images/fog.png',
-                    night: '/images/fog.png',
+                    day: prodPrefix + '/images/fog.png',
+                    night: prodPrefix + '/images/fog.png',
                 }
             },
             48: {
                 description: 'Freezing fog',
                 image: {
-                    day: '/images/fog.png',
-                    night: '/images/fog.png',
+                    day: prodPrefix + '/images/fog.png',
+                    night: prodPrefix + '/images/fog.png',
                 }
             },
             51: {
                 description: 'Light drizzle',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             53: {
                 description: 'Moderate drizzle',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             55: {
                 description: 'Dense drizzle',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             56: {
                 description: 'Freezing drizzle',
                 image: {
-                    day: '/images/hail.png',
-                    night: '/images/hail.png',
+                    day: prodPrefix + '/images/hail.png',
+                    night: prodPrefix + '/images/hail.png',
                 }
             },
             57: {
                 description: 'Freezing drizzle',
                 image: {
-                    day: '/images/hail.png',
-                    night: '/images/hail.png',
+                    day: prodPrefix + '/images/hail.png',
+                    night: prodPrefix + '/images/hail.png',
                 }
             },
             61: {
                 description: 'Slight rain',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             63: {
                 description: 'Moderate rain',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             65: {
                 description: 'Heavy rain',
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             66: {
                 description: 'Freezing Rain', //light
                 image: {
-                    day: '/images/hail.png',
-                    night: '/images/hail.png',
+                    day: prodPrefix + '/images/hail.png',
+                    night: prodPrefix + '/images/hail.png',
                 }
             },
             67: {
                 description: 'Freezing rain', //heavy
                 image: {
-                    day: '/images/hail.png',
-                    night: '/images/hail.png',
+                    day: prodPrefix + '/images/hail.png',
+                    night: prodPrefix + '/images/hail.png',
                 }
             },
             71: {
                 description: 'Light snow',
                 image: {
-                    day: '/images/day-snow.png',
-                    night: '/images/nightsnow.png',
+                    day: prodPrefix + '/images/day-snow.png',
+                    night: prodPrefix + '/images/nightsnow.png',
                 }
             },
             73: {
                 description: 'Moderate snow',
                 image: {
-                    day: '/images/snowflake.png',
-                    night: '/images/snowflake.png',
+                    day: prodPrefix + '/images/snowflake.png',
+                    night: prodPrefix + '/images/snowflake.png',
                 }
             },
             75: {
                 description: 'Heavy snow',
                 image: {
-                    day: '/images/snowflake.png',
-                    night: '/images/snowflake.png',
+                    day: prodPrefix + '/images/snowflake.png',
+                    night: prodPrefix + '/images/snowflake.png',
                 }
             },
             77: {
                 description: 'Snow flurries',
                 image: {
-                    day: '/images/snowflake.png',
-                    night: '/images/snowflake.png',
+                    day: prodPrefix + '/images/snowflake.png',
+                    night: prodPrefix + '/images/snowflake.png',
                 }
             },
             80: {
                 description: 'Rain showers', //light
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             81: {
                 description: 'Rain showers', //moderate
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             82: {
                 description: 'Rain showers', //heavy
                 image: {
-                    day: '/images/rainy.png',
-                    night: '/images/rainy.png',
+                    day: prodPrefix + '/images/rainy.png',
+                    night: prodPrefix + '/images/rainy.png',
                 }
             },
             85: {
                 description: 'Snow showers', //light
                 image: {
-                    day: '/images/snowflake.png',
-                    night: '/images/snowflake.png',
+                    day: prodPrefix + '/images/snowflake.png',
+                    night: prodPrefix + '/images/snowflake.png',
                 }
             },
             86: {
                 description: 'Snow showers', //heavy
                 image: {
-                    day: '/images/snowflake.png',
-                    night: '/images/snowflake.png',
+                    day: prodPrefix + '/images/snowflake.png',
+                    night: prodPrefix + '/images/snowflake.png',
                 }
             },
             95: {
                 description: 'Thunderstorm',
                 image: {
-                    day: '/images/storm.png',
-                    night: '/images/storm.png',
+                    day: prodPrefix + '/images/storm.png',
+                    night: prodPrefix + '/images/storm.png',
                 }
             },
             96: {
                 description: 'Thunderstorm (Hail)', //light
                 image: {
-                    day: '/images/stormhail.png',
-                    night: '/images/stormhail.png',
+                    day: prodPrefix + '/images/stormhail.png',
+                    night: prodPrefix + '/images/stormhail.png',
                 }
             },
             99: {
                 description: 'Thunderstorm (Hail)', //heavy
                 image: {
-                    day: '/images/stormhail.png',
-                    night: '/images/stormhail.png',
+                    day: prodPrefix + '/images/stormhail.png',
+                    night: prodPrefix + '/images/stormhail.png',
                 }
             }
         }
